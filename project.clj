188fc9c5eaa7c96faa601e7a12fca66a3ea9f1a3
Ram Krishnan <rkris@kriyative.net>
2010-07-21 18:29:58 -0700

    Use leiningen for building and publishing to clojars.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.clojars.kriyative/cynojure "1.0.0"
+  :description "Cynojure - clojure library"
+  :url "http://github.com/kriyative/cynojure"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [net.java.dev.jets3t/jets3t "0.7.3"]
+                 [com.google.code.typica/typica "1.7"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]]
+  :aot [cynojure.cl cynojure.util cynojure.sql cynojure.aws])
