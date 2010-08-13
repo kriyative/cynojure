b118739c9436701699bd23e00b810ccfbb7d6c63
Ram Krishnan <rkris@kriyative.net>
2010-08-13 01:10:28 -0700

    Bumped version up to 1.0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kriyative/cynojure "1.0.1"
+(defproject org.clojars.kriyative/cynojure "1.0.2"
   :description "Cynojure - clojure library"
   :url "http://github.com/kriyative/cynojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
