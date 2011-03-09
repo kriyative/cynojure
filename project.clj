ac818d3c6627bd8d630665f4b64f0467fd339048
Ram Krishnan <rkris@kriyative.net>
2011-03-08 22:32:49 -0800

    Added leiningen.hooks.difftest dev-dependency and updated the test-path.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kriyative/cynojure "1.2.1"
+(defproject org.clojars.kriyative/cynojure "1.2.2"
   :description "Cynojure - clojure library"
   :url "http://github.com/kriyative/cynojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
@@ -6,5 +6,7 @@
                  [net.java.dev.jets3t/jets3t "0.7.3"]
                  [com.google.code.typica/typica "1.7"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]]
-  :aot [cynojure.cl cynojure.util cynojure.sql cynojure.aws])
+                     [lein-clojars "0.5.0"]
+                     [lein-difftest "1.3.1"]]
+  :hooks [leiningen.hooks.difftest]
+  :test-path "test")
